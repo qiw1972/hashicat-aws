f235b8a82d8a54138ec16c02a1576a7ba6b0852d
qiw1972 <59665282+qiw1972@users.noreply.github.com>
2020-01-09 15:32:45 -0800

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -10,7 +10,7 @@
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+ Peace!
   <!-- END -->
 
   </div>
