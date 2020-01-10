d3857bd282fc10d9cb7a4d11df0a230f9642c01c
qiw1972 <59665282+qiw1972@users.noreply.github.com>
2020-01-09 16:22:45 -0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,17 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  #source = "terraform-aws-modules/vpc/aws"
+  source  = "app.terraform.io/BigCat/vpc/aws"
+  version = "2.21.0"
 
+  database_subnet_assign_ipv6_address_on_creation = "false"
+  elasticache_subnet_assign_ipv6_address_on_creation = "false"
+  enable_classiclink = "false"
+  enable_classiclink_dns_support = "false"
+  intra_subnet_assign_ipv6_address_on_creation = "false"
+  private_subnet_assign_ipv6_address_on_creation = "false"
+  public_subnet_assign_ipv6_address_on_creation = "false"
+  redshift_subnet_assign_ipv6_address_on_creation = "false"
+  
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
