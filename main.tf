8eae58d44e14906826f3728af73725eb635d5bff
Qi Wang <qiw1972@yahoo>
2020-01-10 00:39:37 +0000

    Added VPC module

--- main.tf
+++ main.tf
@@ -180,6 +180,8 @@ resource tls_private_key "hashicat" {
 
 locals {
   private_key_filename = "${var.prefix}-ssh-key.pem"
+  private_key_filename = "${var.Department}-ssh-key.pem"
+  private_key_filename = "${var.Billable}-ssh-key.pem"
 }
 
 resource aws_key_pair "hashicat" {
